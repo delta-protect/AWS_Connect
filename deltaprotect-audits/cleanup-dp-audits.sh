1cd6e12e41cf72da4d45bb657d6c2e8a6f7d955f
JArmandoG <90565560+JArmandoG@users.noreply.github.com>
2024-03-21 18:14:50 -0600

    Create cleanup-dp-audits.sh

---
+++ deltaprotect-audits/cleanup-dp-audits.sh
@@ -0,0 +1,19 @@
+# Detach policies & Delete Delta Protect Audit IAM Role
+
+ROLE_NAME="deltaprotect-audits"
+ACCOUNT_ID=$(aws sts get-caller-identity --query "Account" --output text)
+
+aws iam detach-role-policy --role-name $ROLE_NAME --policy-arn arn:aws:iam::aws:policy/job-function/ViewOnlyAccess && \
+          echo "[+] 1/5 Successfully dettached policy from role" || echo "[-] Failure dettaching policy from role"
+
+aws iam detach-role-policy --role-name $ROLE_NAME --policy-arn arn:aws:iam::aws:policy/SecurityAudit && \
+          echo "[+] 2/5 Successfully dettached policy from role" || echo "[-] Failure dettaching policy from role"
+
+aws iam detach-role-policy --role-name $ROLE_NAME --policy-arn arn:aws:iam::$ACCOUNT_ID:policy/deltaProtect-customPolicy.json && \
+                        echo "[+] 3/5 Detached policy from role" || echo "[-] Failure detaching policy from role"
+
+aws iam delete-role --role-name $ROLE_NAME && \
+                        echo "[+] 4/5 Successfully deleted role from the acocunt" || echo "[-] Failure deleting role from the account"
+
+aws iam delete-policy --policy-arn arn:aws:iam::$ACCOUNT_ID:policy/deltaProtect-customPolicy.json && \
+                        echo "[+] 5/5 Successfully deleted additions-policy" || echo "[-] Failure deleting additions-policy"
